0ff0caf4253571f5a3fad1f648e5a5ca0f7e411f
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-10-03 11:17:16 -0400

    changes from red to green to blue then white and repeats.

--- main.ts
+++ main.ts
@@ -1,8 +1,39 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created on: Oct 2024
+ * This program changes from red to green to blue then white and repeats.
 */
 
-basic.showString('Hello, World!')
+//setup//
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+pins.digitalWritePin(DigitalPin.P13, 0)
+pins.digitalWritePin(DigitalPin.P14, 0)
+pins.digitalWritePin(DigitalPin.P15, 0)
+
+//Code
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+})
