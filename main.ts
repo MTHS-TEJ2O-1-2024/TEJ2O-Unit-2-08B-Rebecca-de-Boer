0a0b60f89e764f5293d1168fc55c17c2dc2866d9
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-10-23 11:00:32 -0400

    shows colors of the rainbow

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
+ * Created by: Rebecca de Boer
  * Created on: Oct 2024
  * This program changes from red to green to blue then white and repeats.
 */
@@ -12,7 +12,7 @@ pins.digitalWritePin(DigitalPin.P13, 0)
 pins.digitalWritePin(DigitalPin.P14, 0)
 pins.digitalWritePin(DigitalPin.P15, 0)
 
-//Code
+// shows the colors of the rainbow
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
 
@@ -31,11 +31,46 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P14, 0)
     pause(1000)
 
-basic.showIcon(IconNames.Yes)
-pins.digitalWritePin(DigitalPin.P15, 1)
+    // green
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pause(1000)
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P15, 0)
 
-pause(1000)
+    // yellow
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pause(1000)
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
 
-basic.showIcon(IconNames.No)
-pins.digitalWritePin(DigitalPin.P15, 0)
+    // purple
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pause(1000)
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+
+    // yellow
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pause(1000)
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    // orange
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pause(1000)
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
 })
